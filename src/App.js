5cf38aedf266f191e10665779f3f13c952490bbd
kukuhbr <kukuhis777@gmail.com>
2019-05-07 01:53:32 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Kukuh Basuki Rahmat!
         </p>
         <a
           className="App-link"
