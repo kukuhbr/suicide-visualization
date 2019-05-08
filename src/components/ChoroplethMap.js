221180e8885e20cae95dbcaedfae68c2b40ed128
kukuhbr <kukuhis777@gmail.com>
2019-05-08 18:13:12 +0700

    repair map to show all

--- src/components/ChoroplethMap.js
+++ src/components/ChoroplethMap.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import Datamap from 'datamaps/dist/datamaps.world.min.js';
-import d3 from 'd3';
+import * as d3 from 'd3';
 
 class ChoroplethMap extends Component {
   componentDidMount() {
     let dataset = {};
-    let map = new Datamap({element: document.getElementById('container')});
+    let map = new Datamap({
+      element: document.getElementById('container'),
+      setProjection: function (element) {
+        var projection = d3.geoMercator()
+          .scale(100)
+          .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+
+        var path = d3.geoPath().projection(projection);
+        return { path: path, projection: projection };
+      }
+    });
   }
 
   render() {
     return (
       <div id="container" style={{
-        height: "100%",
-        width: "100%",
+        height: "100vh",
+        width: "100vw",
       }}>
       </div>
     );
